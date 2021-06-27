b1fd815890b2af6d297d3983ea6eb3a8920e0465
Theo Nejm <theoonejm@gmail.com>
2021-06-26 22:46:13 -0300

    Prepair to deploy project on heroku

--- src/server.js
+++ src/server.js
@@ -14,4 +14,4 @@ server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, ()=>console.log('rodando'))
+server.listen(process.env.PORT || 3000, () => console.log("RUNNING..."));
